bfffb69272de06868c53992ec74dc4d0adda14b4
DenI-10 <121663755+DenI-10@users.noreply.github.com>
2023-01-16 21:59:49 +0300

    Task_007

---
+++ Task_007/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напиши программу, 
+// 1.которая принимает на вход трехзначные число 
+// 2. и на выходе показывает последнию цифру этого числа
+// 3.вывод результата
+
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Clear();
+Console.WriteLine("Задача 10 ");
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
