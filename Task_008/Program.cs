123e1955368d78b550047b586a760062b67b02b9
DenI-10 <121663755+DenI-10@users.noreply.github.com>
2023-01-16 22:47:58 +0300

    Task_008

---
+++ Task_008/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, 
+//  1.которая на вход принимает число (N), 
+//  2.а на выходе показывает все чётные числа от 1 до N.
+//  Напиши вывод.
+
+//  5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
