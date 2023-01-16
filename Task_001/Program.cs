5a0e14e1c0bbb8ea39721b49a7c3a1cc0d358e26
DenI-10 <121663755+DenI-10@users.noreply.github.com>
2023-01-16 21:44:45 +0300

    Task_001

---
+++ Task_001/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напиши программу, которая 
+// 1.на вход принимает два числа 
+// 2.и проверяет, 
+// 3.являеться ли первое число квадратом второго.
+// 4.вывод результата.
+
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3, b = 9 -> нет
+
+Console.WriteLine("введите целое число 1: " );
+int number = Convert.ToInt32(Console.ReadLine());// 3434
+Console.WriteLine("введите целое число 2: " );
+int number1 = Convert.ToInt32(Console.ReadLine());// 3434
+if (number1 * number1 == number)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
