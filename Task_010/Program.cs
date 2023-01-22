89725ffcf6335154c0fcb0b89c58055c48d6589c
DenI-10 <121663755+DenI-10@users.noreply.github.com>
2023-01-22 17:52:13 +0300

    Task_010

---
+++ Task_010/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, 
+//1.которая принимает на вход трёхзначное число 
+//2.и на выходе показывает вторую цифру этого числа.
+//3.напишите вывод
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехначное целое число "); //ввод текста
+try {
+	int Value= Convert.ToInt32(Console.ReadLine());
+	int Length = Value.ToString().Length;
+	if (Length == 3) {
+		int result = ( Value/10 ) % 10;
+		Console.WriteLine($"Вторая цифра {result}");
+	} else {
+		Console.WriteLine("Нет трехзначного числа");
+	}
+} catch (Exception e) {
+	Console.WriteLine(e.Message);
+}
