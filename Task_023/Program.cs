a79140d94c6b845eaad3f03ff404564c9441e226
DenI-10 <121663755+DenI-10@users.noreply.github.com>
2023-01-25 19:13:14 +0300

    Task_023

---
+++ Task_023/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, 
+// 1.которая принимает на вход число (N) 
+// 2.и выдаёт таблицу кубов чисел от 1 до N.
+// 3.напишите вывод
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int number = ReadInt("Введите число: ");
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i * i * i}, ");
+}
+
+// Метод
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
